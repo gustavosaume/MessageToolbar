99483a23ef7776b97ebbd543d096ce60f3b9e030
Gustavo Saume <gustavosaume@gmail.com>
2017-01-25 12:32:49 -0500

    Improves sample vc to test interactions

--- MessageToolbar/MessageToolbar/ViewController.swift
+++ MessageToolbar/MessageToolbar/ViewController.swift
@@ -9,8 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    fileprivate static let cellID = "cellID"
 
     // MARK: - Variables
+
+    fileprivate lazy var table: UITableView = {
+        $0.dataSource = self
+        $0.delegate = self
+        $0.frame = self.view.bounds
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: ViewController.cellID)
+        $0.keyboardDismissMode = .onDrag
+        return $0
+    }(UITableView())
 
     fileprivate lazy var toolbar: MessageToolbar = {
         let toolbar = MessageToolbar(frame: CGRect(x: 0.0, y: 0.0, width: 0.0, height: 44.0))
@@ -19,7 +29,8 @@ class ViewController: UIViewController {
     }()
 
     override var inputAccessoryView: UIView? {
-        return toolbar
+        let shouldHideToolbar = presentedViewController == nil || presentedViewController?.isBeingDismissed ?? false
+        return shouldHideToolbar ? toolbar : nil
     }
 
     override var canBecomeFirstResponder: Bool {
@@ -48,9 +59,39 @@ class ViewController: UIViewController {
         btn3.sizeToFit()
 
         toolbar.leftButtons = [btn1, btn2, btn3]
+        toolbar.delegate = self
 
-//        view.addSubview(toolbar)
-//        toolbar.frame.size.width = UIScreen.main.bounds.width
-//        toolbar.frame = toolbar.frame.offsetBy(dx: 0.0, dy: UIScreen.main.bounds.height - 44.0)
+        view.addSubview(table)
+    }
+}
+
+extension ViewController: MessageToolbarDelegate {
+    func sendButtonPressed() {
+        print("pressed")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1_000
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ViewController.cellID, for: indexPath)
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        resignFirstResponder()
+        toolbar.resignFirstResponder()
+        view.endEditing(true)
+
+        let alert = UIAlertController(title: "Selected", message: String(indexPath.row), preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
 }
